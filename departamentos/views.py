cc359dd4b715dc8c745a9e71d2bdab063a9322b9
A07971.github <aderitoborgesmonteiro.bb@gmail.com>
2025-05-21 00:00:31 -0100

    gestao equipamento erro

--- departamentos/views.py
+++ departamentos/views.py
@@ -351,7 +351,7 @@ def get_equipamento(request):
                           equipament = [dict(zip(colunas, row)) for row in cursor.fetchall()]
                           print(equipament)
                      
-                      return JsonResponse(serialize("json", equipament),safe=False)
+                          return JsonResponse(serialize("json", equipament),safe=False)
 
       except Exception as e:
              return JsonResponse({'status': 'error', 'message': str(e)}, status=400)
